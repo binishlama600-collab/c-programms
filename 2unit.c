b3dbeaa2a22bc2d18d2905449557f7cc93afbf5e
unknown <binishlama600@gmail.com>
2026-01-06 08:15:45 +0545

    KDBC

---
+++ 2unit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int age=1;
+    int roll =10;
+    int salary =100;
+float pi =46.468989;
+float api=46.79898999999;
+float gpa=3.25898989999;
+
+    printf("%-2d\n",age);
+    printf("%-2d\n", roll);
+    printf("%-2d\n", salary);
+    printf("%-7.2f\n",pi);
+    printf("%-7.3f\n",api);
+    printf("%-7.5f\n",gpa);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+
+ // variable = A reusable container for a valur.
+ //                behaves as if it were the value contains.
+ // int age =%d;
+ // float for reminder numbers =%.lf or %.2f
+ // char for single characters =%c
+ // char array (string) for multiple characters =%s
+ // bool for boolean values (true of false) =%d     for use of bool requires<#include<stdbool.h>
+//width specifiers %4d 
+
+
+
+
+
